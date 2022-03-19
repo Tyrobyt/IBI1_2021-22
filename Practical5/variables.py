1d78fe809e7fb7313ce2d69d5d0cf2c849178dd5
Tyrobyt <3210110184@10.105.100.153>
2022-03-19 19:26:44 +0800

    Practical5/

---
+++ Practical5/variables.py
@@ -0,0 +1,13 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+# d = the rate of new cases in 2020
+e=a-b
+# e = the rate of new cases in 2021
+if (d-e>0):
+  print ("d>e, the rate of new cases in 2020 is greater than 2021")
+elif (d-e==0):
+  print ("d=e")
+else:
+  print ("d<e, the rate of new cases in 2021 is greater than 2020")
