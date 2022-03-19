d05faf742d195d0a3f02df64d713434d34f6fa15
Tyrobyt <3210110184@10.105.100.153>
2022-03-19 21:40:47 +0800

    Practical5/

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,13 +1,18 @@
 #p = the number of pizza piece, n = the number of cuts
 #we have 64 people in IBI1 class
-#aim: calculate the number of cut(n) to make 64 piece(p=64)
+#aim: calculate the number of cut(n) to make the number of piece>=64
+#using while loop and if 
+# if the number < 64, then print the number of piece and cut
+# if the number >=64, then print the number of piece and cut, and add the word to indicate that we get enough piece.
 
 p=0
 n=0
 while p<64:
  p=(n**2+n+2)/2
  if(p>64):
-     print n
+     print 'when the number of cut =',n,', the number of piece =',p,'we get enough piece of pizza for each member!'
  else:
+     print 'when the number cut =',n,', the number of piece =',p
      n+=1
+     
 
